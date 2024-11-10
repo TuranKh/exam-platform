d340419a9848edbc1b7c5b889fc90cdaaece1212
Turan Khidilov <turan.khidilov@gmail.com>
2024-11-10 15:06:53 +0400

    fix: typo

--- src/service/StorageService.ts
+++ src/service/StorageService.ts
@@ -1,9 +1,9 @@
 import { supabase } from "@/supabase/init";
 
 export default class StorageService {
-  static async uploadFileToStorage(file: File, deafultStorage = "questions") {
+  static async uploadFileToStorage(file: File, defaultStorage = "questions") {
     const { data, error } = await supabase.storage
-      .from(deafultStorage)
+      .from(defaultStorage)
       .upload(file.name, file, {
         cacheControl: "3600",
         upsert: false,
