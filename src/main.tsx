f71705efa005258247b2b011c69b17b9410e00fd
Turan Khidilov <turan.khidilov@gmail.com>
2024-12-22 13:26:07 +0400

    fix: wrong bulk operation

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ const queryClient = new QueryClient({
   },
 });
 
-createRoot(document.getElementById("root")!).Render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
