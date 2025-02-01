ab5112bd6209bc1496fcf33d7c6410e290c3964f
Turan Khidilov <turan.khidilov@gmail.com>
2025-02-02 00:09:39 +0400

    feat: add openai setup for further integrations

---
+++ src/service/OpenAIService.ts
@@ -0,0 +1,20 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  dangerouslyAllowBrowser: true,
+});
+
+export const completion = async function () {
+  const result = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      { role: "system", content: "You are a helpful assistant." },
+      {
+        role: "user",
+        content: "Write a haiku about recursion in programming.",
+      },
+    ],
+    store: true,
+  });
+  console.log(result);
+};
