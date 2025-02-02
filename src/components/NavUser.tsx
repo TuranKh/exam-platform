767d4cd58bbe1e5398c10c24eb7e053f33f3e826
Turan Khidilov <turan.khidilov@gmail.com>
2025-02-02 19:09:48 +0400

    feat: replace avatar with image from google

--- src/components/NavUser.tsx
+++ src/components/NavUser.tsx
@@ -72,7 +72,10 @@ export default function NavUser() {
               className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'
             >
               <Avatar className='h-8 w-8 rounded-lg'>
-                <AvatarImage src={"no-image"} alt={"Temporary"} />
+                <AvatarImage
+                  src={userDetails?.profileImageUrl}
+                  alt={"Temporary"}
+                />
                 <AvatarFallback className='rounded-lg'>
                   {initials}
                 </AvatarFallback>
