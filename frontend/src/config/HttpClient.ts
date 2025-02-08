e67ef332d3ee6bd91b802e0671b9b65e3a6a041a
Turan Khidilov <turan.khidilov@gmail.com>
2025-02-08 23:37:05 +0400

    chore: setup backend url & adopt httpClient for current backend

--- frontend/src/config/HttpClient.ts
+++ frontend/src/config/HttpClient.ts
@@ -1,14 +1,6 @@
 import axios from "axios";
+import { config } from "../../env";
 
-export const httpClient = axios.create();
-
-httpClient.interceptors.request.use(
-  function (config) {
-    // Do something before request is sent
-    return config;
-  },
-  function (error) {
-    // Do something with request error
-    return Promise.reject(error);
-  },
-);
+export const httpClient = axios.create({
+  baseURL: config.backend.url,
+});
