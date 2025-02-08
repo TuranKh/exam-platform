5476583f63a13f33c1d70e777bb9e886529232df
Turan Khidilov <turan.khidilov@gmail.com>
2025-02-09 00:02:34 +0400

    feat: add utility path for shared files

---
+++ backend/src/supabase/supabase.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, OnApplicationBootstrap } from "@nestjs/common";
+import config from "@shared/env";
+import { createClient } from "@supabase/supabase-js";
+
+@Injectable()
+export class SupabaseService implements OnApplicationBootstrap {
+  onApplicationBootstrap() {
+    this.init();
+  }
+
+  init() {
+    createClient(config.supabase.url, config.supabase.clientKey);
+    console.log("executed");
+  }
+}
